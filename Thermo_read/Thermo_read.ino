2758935126adbc34860c18a7bf7dcb5751627179
RyoKanno <tdu.aqa1@gmail.com>
2024-06-06 10:37:45 +0200

    Thermo sensor integrated

---
+++ Thermo_read/Thermo_read.ino
@@ -0,0 +1,69 @@
+/***************************************************
+  This is an example for the Adafruit Thermocouple Sensor w/MAX31855K
+
+  Designed specifically to work with the Adafruit Thermocouple Sensor
+  ----> https://www.adafruit.com/products/269
+
+  These displays use SPI to communicate, 3 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+
+// Default connection is using software SPI, but comment and uncomment one of
+// the two examples below to switch between software SPI and hardware SPI:
+
+// Example creating a thermocouple instance with software SPI on any three
+// digital IO pins.
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
+
+// initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+
+// Example creating a thermocouple instance with hardware SPI
+// on a given CS pin.
+//#define MAXCS   10
+//Adafruit_MAX31855 thermocouple(MAXCS);
+
+// Example creating a thermocouple instance with hardware SPI
+// on SPI1 using specified CS pin.
+//#define MAXCS   10
+//Adafruit_MAX31855 thermocouple(MAXCS, SPI1);
+
+void setup() {
+  Serial.begin(115200);
+
+  while (!Serial) delay(1); // wait for Serial on Leonardo/Zero, etc
+
+  if (!thermocouple.begin()) {
+    Serial.println("ERROR.");
+    while (1) delay(10);
+  }
+
+  // OPTIONAL: Can configure fault checks as desired (default is ALL)
+  // Multiple checks can be logically OR'd together.
+  // thermocouple.setFaultChecks(MAX31855_FAULT_OPEN | MAX31855_FAULT_SHORT_VCC);  // short to GND fault is ignored
+
+  Serial.println("DONE.");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String inputString = Serial.readStringUntil('\n');
+    if (inputString == "Reference Temperature") {
+      Serial.println(thermocouple.readInternal());
+    } else if (inputString == "Probe Temperature") {
+      Serial.println(thermocouple.readCelsius());
+    }
+  }
+  delay(100);
+}
